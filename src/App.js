38e3dda9aaf76445b4dccb0124b3ea57ca28803a
DpkRn <dwizardtechno@gmail.com>
2024-09-10 04:08:07 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import Login from "./pages/login/Login";
-// import Signup from "./pages/signup/Signup";
+
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Signup/> */}
       <Login/>
     </div>
   );
